e52b66096bd3ab0d358b5dd36e8de2dac24feb75
shazidongdong <923497902@qq.com>
2019-01-21 18:08:58 +0800

    0.43

--- src/main/java/com/hm/controller/UserController.java
+++ src/main/java/com/hm/controller/UserController.java
@@ -6,10 +6,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.hm.entity.User;
+import com.hm.service.IUserRoleService;
 import com.hm.service.IUserService;
 
 /**
@@ -25,9 +27,22 @@ import com.hm.service.IUserService;
 public class UserController {
 	@Autowired
 	IUserService userService;
+	@Autowired
+	IUserRoleService UserRoleService;
+	
 	@GetMapping("/list")
 	@ResponseBody
 	public List<User> getAll(){
 	 return	userService.getALLUserAndRole();
+	}
+	
+	@GetMapping("/del/{id}")
+	@ResponseBody
+	public String delById(@PathVariable String id) {
+		System.out.println(id);
+		Integer id1 = Integer.valueOf(id);
+		userService.removeById(id1);
+		UserRoleService.removeById(id1);
+		return "success";
 	}
 }
--- src/test/java/com/hm/Utest.java
+++ src/test/java/com/hm/Utest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.hm.service.IUserRoleService;
 import com.hm.service.IUserService;
 
 @RunWith(SpringRunner.class)
@@ -13,8 +14,18 @@ import com.hm.service.IUserService;
 public class Utest {
 	@Autowired
 	IUserService UserService;
+	@Autowired
+	IUserRoleService UserRoleService;
+	
 	@Test
 	public void findmenuById() {
 		UserService.findMenuById(1);
+	}
+	@Test
+	public void delById() {
+		Integer id =22;
+		UserService.removeById(id);
+		UserRoleService.removeById(id);
+		System.out.println("sss");
 	}
 }
